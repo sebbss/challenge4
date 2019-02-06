808aa166288435eddf5a7d505e421ee3ca27d5cb
sebbss <jsenabulya2@gmail.com>
2019-02-06 16:01:30 +0300

    [finishes #163753559] add user login feature

---
+++ js/login.js
@@ -0,0 +1,48 @@
+window.onload = function signup () {
+	document.getElementById('loginF').addEventListener('submit', login)
+
+	function login (e) {
+		e.preventDefault()
+		var username = document.getElementById('username').value;
+		var password = document.getElementById('psw').value;
+
+		var logindata = {
+					username:username,
+					password:password
+		};
+		
+
+	fetch('http://127.0.0.1:5000/login', {
+			method:'POST',
+			mode:'cors',
+			headers:{
+				'content-Type':'application/json',
+				'Accept':'application/json'
+			},
+			body:JSON.stringify(logindata)
+	}).then(res => res.json())
+	.then(response =>{
+		console.log(response['data'][0]['token']);
+		if (response['status'] == 200 && response['data'][0]['user']['isAdmin']==true){
+				redirect:window.location.replace('admin.html')
+			tkn = response['data'][0]['token'];
+			localStorage.setItem('access', tkn);
+			localStorage.setItem('user', username);
+			}
+			else if (response['status'] == 200 && response['data'][0]['user']['isAdmin']=='False') {
+				redirect:window.location.replace('user.html')
+			tkn = response['data'][0]['token'];
+			localStorage.setItem('access', tkn);
+			localStorage.setItem('user', username);
+			}
+			else{
+				alert(response['message'])
+				redirect:window.location.replace('#')
+			};
+	})
+
+	}
+
+
+}
+
