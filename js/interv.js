abb08c96213a40e51f286bea82e2cbcd939c6b20
sebbss <jsenabulya2@gmail.com>
2019-02-08 15:39:49 +0300

    [Delivers #163782099] add file upload

--- js/interv.js
+++ js/interv.js
@@ -5,3 +5,48 @@ function displayinterv(){
 span.onclick = function(){
     document.getElementById('modal-interv').style.display = "none";
 }
+
+var url = "http://127.0.0.1:5000/ireporter/api/v2/intervention"
+var token = localStorage.getItem('access')
+
+document.getElementById('modal-interv').addEventListener('submit', create_intervention)
+
+
+//create intervention
+
+
+function create_intervention(e) {
+
+	e.preventDefault()
+	var location = document.getElementById('location').value;
+	var description = document.getElementById('description').value;
+	var image = document.getElementById('image').value;
+	var video = document.getElementById('video').value;
+
+	var inter_data = {
+		location:location,
+		description:description,
+		image:image,
+		video:video,
+	} 
+
+	fetch(url,{
+		method:'POST',
+		mode:'cors',
+		headers:{
+				'content-Type':'application/json',
+				'Accept':'application/json',
+				Authorization:`Bearer ${token}`
+			},
+		body:JSON.stringify(inter_data)
+	})
+	.then(res => res.json())
+	.then(data =>{
+		if (data['status']==201){
+			alert('created an intervention')
+		}
+		else{
+			alert(data['message'])
+		}
+	})
+}
